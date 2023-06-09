0f01ed052ab403c8dfa70ac7c59d9bfe75ebd999
Adoyo Clifford <adoyoclifford@gmail.com>
2023-06-09 10:18:28 +0300

    initial commit

--- app/src/main/java/com/adoyo/tictactoe/presentation/TicTacToeViewModel.kt
+++ app/src/main/java/com/adoyo/tictactoe/presentation/TicTacToeViewModel.kt
@@ -3,13 +3,17 @@ package com.adoyo.tictactoe.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.adoyo.tictactoe.data.GameState
+import com.adoyo.tictactoe.data.MakeTurn
 import com.adoyo.tictactoe.data.RealTimeMessaging
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import java.net.ConnectException
 import javax.inject.Inject
 
 class TicTacToeViewModel @Inject constructor(
@@ -19,6 +23,7 @@ class TicTacToeViewModel @Inject constructor(
         .gameStateStream()
         .onStart { _isConnecting.value = true }
         .onEach { _isConnecting.value = false }
+        .catch { t-> _showConnectionError.value = t is ConnectException }
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), GameState())
 
     private val _isConnecting = MutableStateFlow(false)
@@ -26,4 +31,21 @@ class TicTacToeViewModel @Inject constructor(
 
     private val _showConnectionError = MutableStateFlow(false)
     val showConnectionError = _showConnectionError.asStateFlow()
+
+    fun finishTurn(x: Int, y: Int) {
+        if (state.value.field[y][x] != null || state.value.winningPlayer != null) {
+            return
+        }
+
+        viewModelScope.launch {
+            client.sendAction(MakeTurn(x,y))
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelScope.launch {
+            client.close()
+        }
+    }
 }
